3b4ab0e97bbb38a8120b5c73e32716337b4acb74
Nathaniel Downey <downeyn21@gmail.com>
2025-06-18 13:57:50 -0400

    changed format

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,19 +2,24 @@ function Card() {
   return ( 
     <>
       <div className="cardContainer">
-        <div className="cardInfoWrapper">
-          <div className="cardNameWrapper">
-            First Name, Last Name
-          </div>
-          <div className="cardDescWrapper">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui error libero nisi assumenda modi possimus, fuga odit laboriosam, nulla odio voluptates blanditiis tempora quod iste molestias, vero esse tenetur reprehenderit.
-          </div>
-          <button>
-            Follow
-          </button>
+        <div className="cardTitle">
+          Captain License
         </div>
-        <div className="imgContainer">
-          <img src="/captainSpongeBub.png" alt="CapPfp" />
+        <div className="cardWrapper">
+          <div className="cardInfoWrapper">
+            <div className="cardNameWrapper">
+              First Name, Last Name
+            </div>
+            <div className="cardDescWrapper">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui error libero nisi assumenda modi possimus, fuga odit laboriosam, nulla odio voluptates blanditiis tempora quod iste molestias, vero esse tenetur reprehenderit.
+            </div>
+            <button>
+              Follow
+            </button>
+          </div>
+          <div className="imgContainer">
+            <img src="/captainSpongeBub.png" alt="CapPfp" />
+          </div>
         </div>
       </div>
     </>
